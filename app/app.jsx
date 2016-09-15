f3dac2082ce964bf19ecc863c643efef35f32380
enrique.munguia <enrique.munguia@hulu.com>
2016-09-14 19:55:09 -0500

    add airbnb eslint config

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,5 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <h1>Boilerplate App!</h1>,
