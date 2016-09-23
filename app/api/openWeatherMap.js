cd48359d502fb8a7019bb6c84512ad2ee864d2ad
enrique.munguia <enrique.munguia@hulu.com>
2016-09-22 22:05:34 -0500

    add loading indicator

--- app/api/openWeatherMap.js
+++ app/api/openWeatherMap.js
@@ -4,8 +4,8 @@ const WEATHER_API_KEY = 'c2389f8342748200b6a1cb23ed83fe1f';
 const OPEN_WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
 module.exports = {
-  getTemperature(location) {
-    const encodedLocation = encodeURIComponent(location);
+  getTemperature(city) {
+    const encodedLocation = encodeURIComponent(city);
     const requestUrl = `${OPEN_WEATHER_BASE_URL}?q=${encodedLocation}&APPID=${WEATHER_API_KEY}&units=metric`;
 
     return axios.get(requestUrl)
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -7,26 +7,38 @@ class Weather extends React.Component {
   constructor() {
     super();
     this.state = {
-      city: 'Mexico City',
-      temp: 20
+      isLoading: false
     };
     this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleSearch(city) {
+    this.setState({ isLoading: true });
     OpenWeatherMap.getTemperature(city)
       .then((temp) => {
-        this.setState({ city, temp });
+        this.setState({ city, temp, isLoading: false });
       }, (error) => {
         console.log(error);
+        this.setState({ isLoading: false });
       });
+  }
+
+  showMessage() {
+    const { isLoading, temp, city } = this.state;
+    if (isLoading) {
+      return <h3>Fetching weather...</h3>;
+    } else if (temp && city) {
+      return <WeatherMessage city={city} temperature={temp} />;
+    }
+
+    return null;
   }
 
   render() {
     return (
       <div>
         <WeatherForm onSearch={this.handleSearch} />
-        <WeatherMessage city={this.state.city} temperature={this.state.temp} />
+        {this.showMessage()}
       </div>
     );
   }
