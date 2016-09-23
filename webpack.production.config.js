d2d00badf0e34de8f455d50386365de9e327b18f
enrique.munguia <enrique.munguia@hulu.com>
2016-09-22 23:38:14 -0500

    add webpack production config

---
+++ webpack.production.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-source-map'
+};
