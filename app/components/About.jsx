74dc6f1b9220ca146144c71c391537a7c1710e46
enrique.munguia <enrique.munguia@hulu.com>
2016-09-23 20:59:47 -0500

    add styles to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => (
-  <h3>About Component</h3>
+  <div>
+    <h1 className="text-center">About</h1>
+    <p>Weather application built with <a href="https://facebook.github.io/react/" target="_blank" rel="noopener noreferrer">React JS</a> and <a href="https://openweathermap.org/" target="_blank" rel="noopener noreferrer">OpenWeatherMap</a></p>
+    <p>Check it on <a href="https://github.com/enrique7mc/ReactWeather" target="_blank" rel="noopener noreferrer">Github</a></p>
+    <p>Twitter: <a href="https://twitter.com/enrique7mc_">@enrique7mc_</a></p>
+  </div>
 );
 
 module.exports = About;
