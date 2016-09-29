7a76a361460fdfa58f5e2612abe9c186cd37428f
enrique.munguia <enrique.munguia@hulu.com>
2016-09-28 21:13:37 -0500

    fix modal error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,21 +1,33 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 export default class ErrorModal extends Component {
   componentDidMount() {
-    const modal = new Foundation.Reveal($('#error-modal')); // eslint-disable-line no-undef
-    modal.open();
-  }
-
-  render() {
     const { title, message } = this.props;
-    return (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+    const modalMarkup = (
+      <div
+        id="error-modal"
+        className="reveal tiny text-center"
+        data-reveal=""
+        ref={node => (this.node = node)}
+      >
         <h4>{title}</h4>
         <p>{message}</p>
         <p>
           <button className="button hollow" data-close="">Okay</button>
         </p>
       </div>
+    );
+
+    const $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(this.node).html($modal);
+    const modal = new Foundation.Reveal($('#error-modal')); // eslint-disable-line no-undef
+    modal.open();
+  }
+
+  render() {
+    return (
+      <div ref={node => (this.node = node)} />
     );
   }
 }
